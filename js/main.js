7f2738aab0ab2eebee4ea642afc5315d4acef85d
vinesk <vinesk.dev@gmail.com>
2023-11-09 16:22:50 +0100

    Clean the function related to the previous commit

--- js/main.js
+++ js/main.js
@@ -71,14 +71,13 @@ const handleButtonClick = (value) => {
   } else if (
     ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(value)
   ) {
-    if (operator) {
-      firstOperand === parseFloat(currentNumber)
-        ? (currentNumber = value)
-        : (currentNumber += value);
+    if (
+      currentNumber === "0" ||
+      (operator && firstOperand === parseFloat(currentNumber))
+    ) {
+      currentNumber = value;
     } else {
-      currentNumber === "0"
-        ? (currentNumber = value)
-        : (currentNumber += value);
+      currentNumber += value;
     }
   } else if (["+", "-", "x", "/"].includes(value)) {
     if (operator) {
